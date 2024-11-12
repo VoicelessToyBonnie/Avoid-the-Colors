d0972289011a3af27916b19006472f8bdd2ec5df
Vinay Munagapati <vxm200033@utdallas.edu>
2024-11-11 21:18:52 -0600

    Created the particle trails, particle, and player class with the initiliazation functions.

--- src/project.py
+++ src/project.py
@@ -2,6 +2,28 @@ import pygame
 import time
 import random
 
+class particleTrail:
+     def __init__(self, pos, size, life, direction):
+        self.pos = pos
+        self.size = size
+        self.life = life
+        self.particles = []
+        self.direction = direction
+
+class Particle:
+    def __init__(self, pos, size, life, color):
+        self.pos = pos
+        self.size = size
+        self.color = color
+        self.age = 0
+        self.life = life
+        self.dead = False
+
+class Player:
+     def __init__(self, pos, size=20):
+        self.pos = pos
+        self.size = size
+        self.color = (255, 255, 255)
 
 def main():
     pygame.init()
