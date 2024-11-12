76afb115120b28b469facd6e4b4e248e4c484cc6
Vinay Munagapati <vxm200033@utdallas.edu>
2024-11-11 20:49:41 -0600

    Setted up the screen resoltuion in main function.

--- src/project.py
+++ src/project.py
@@ -6,7 +6,12 @@ import random
 def main():
     pygame.init()
 
+    screen_res = (1920, 1080)
+    FPS = 60
 
+    screen = pygame.display.set_mode(screen_res, pygame.FULLSCREEN)
+    pygame.display.set_caption("Avoid the Colors")
+    clock = pygame.time.Clock()
 
     pygame.quit()
 
